9e898a3f208195cbdea494614ae6ed9c1ca227e3
YuQiJaang <rdjksp@gmail.com>
2024-10-16 15:00:06 +0800

    add 退火算法

--- src/solve_by_random_algorithm/test.rs
+++ src/solve_by_random_algorithm/test.rs
@@ -1,18 +1,97 @@
 use crate::types::{BackpackContext, Solver};
+use std::ops::Sub;
+use rand::{random, thread_rng, Rng};
+use rand::rngs::ThreadRng;
+
+const START_TMP: f64 = 300.0;
+const END_TMP: f64 = 2.0;
+const TMP_DOWN: f64 = 0.85;
+const EXP: f64 = 1e-2;
+const REV_BITS: i32 = 2;
+const BALANCE_TIME: i32 = 100;
 
 pub struct RandomSolver {
     ctx: BackpackContext,
+    tmp: f64,
+    rand: ThreadRng,
+}
+impl RandomSolver {
+    fn af(&mut self) -> bool {
+        self.tmp *= TMP_DOWN;
+        !(self.tmp < END_TMP + EXP)
+    }
+    fn zero(&self) -> u128 {
+        (0..self.ctx.value.len()).fold(0u128, |g, _x| (g << 1) | 1)
+    }
+    fn weigh_of(&self, v: u128) -> i32 {
+        let mut res = 0;
+        let mut mov = v;
+        let mut idx = 0usize;
+        while mov > 0 {
+            if mov & 1 == 1 {
+                res += self.ctx.weighs[idx];
+            }
+            mov >>= 1;
+            idx += 1;
+        }
+        res
+    }
+    fn value_of(&self, v: u128) -> i32 {
+        let mut res = 0;
+        let mut mov = v;
+        let mut idx = 0usize;
+        while mov > 0 {
+            if mov & 1 == 1 {
+                res += self.ctx.value[idx];
+            }
+            mov >>= 1;
+            idx += 1;
+        }
+        res
+    }
+    fn rand_rev(&mut self, v: u128) -> u128 {
+        let mut r = v;
+        for _time in 0..REV_BITS {
+            let idx = self.rand.gen_range(0..self.ctx.value.len() as i32);
+            r = (r & !(1 << idx)) | ((!r) & (1 << idx));
+        }
+        r
+    }
 }
 impl Solver for RandomSolver {
     fn make(ctx: BackpackContext) -> Self {
-        todo!()
+        RandomSolver {
+            ctx,
+            tmp: START_TMP,
+            rand: thread_rng(),
+        }
     }
 
     fn name() -> &'static str {
-        todo!()
+        "SimulatedAnnealingStochasticAlgorithmRandomSolver"
     }
 
     fn solve(&mut self) -> i32 {
-        todo!()
+        let mut res = 0;
+        let mut top_of = 0;
+        let mut select = random::<u128>() & self.zero();
+        let mut range = 0;
+        loop {
+            select = self.rand_rev(select);
+            if self.weigh_of(select) <= self.ctx.capacity {
+                let v = self.value_of(select);
+                if v >= top_of || random::<f64>() < ((v - top_of) as f64 / self.tmp).exp() {
+                    top_of = v;
+                }
+                res = res.max(top_of);
+            }
+            if range + 1 == BALANCE_TIME {
+                range = 0;
+                if !self.af() {
+                    break;
+                }
+            } else { range += 1; }
+        }
+        res
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -5,7 +5,7 @@ pub fn run() {
     use crate::solve_by_branch_and_bound::test::BranchAndBoundSolver;
     use crate::solve_by_dynamic_programming::test::DynamicProgrammingSolver;
     use crate::solve_by_random_algorithm::test::RandomSolver;
-    SolverContext::<BranchAndBoundSolver>::make(BackpackContext::make(
+    SolverContext::<RandomSolver>::make(BackpackContext::make(
         70,
         vec![71, 69, 1],
         vec![100, 1, 2],
